0f26092c3421a06fd4120d14773d43fd7405e022
Carmela <carmela881@outlook.com>
2023-07-04 20:56:46 +0100

    Adds utils.js file. Requires jsonwebtoken. Creates and exports signToken function

---
+++ server/utils/auth.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+
+const secret = 'mysecretssshhhhhhh';
+const expiration = '2h';
+
+module.exports = {
+  signToken: function ({ email, name, _id }) {
+    const payload = { email, name, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};

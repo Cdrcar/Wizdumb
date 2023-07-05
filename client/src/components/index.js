aa54c3065136b40f4557109c6cfe3d3c6f9fa928
[Iman Jama] <[Iman.jama891@gmail.com]>
2023-07-05 16:27:03 +0100

    Installed tailwind CSS package, updated tailwind.config file and created disst folder for CSS output

---
+++ client/src/components/index.js
@@ -0,0 +1,23 @@
+import AccountSettings from './AccountSettings';
+import Course from './Course';
+import DiscussionPage from './DiscussionPage';
+import Footer from './Footer';
+import Homepage from './Homepage';
+import LoggedinHomepage from './LoggedinHomepage';
+import Login from './login';
+import Modules from './modules';
+import Navbar from './Navbar';
+import SearchPage from './SearchPage';
+
+export {
+  AccountSettings,
+  Navbar,
+  DiscussionPage,
+  Homepage,
+  LoggedinHomepage,
+  Login,
+  Footer,
+  Modules,
+  SearchPage,
+  Course,
+}

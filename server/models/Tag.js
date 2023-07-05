43ceb7b3a65580ac273e7a871ed43a182d377992
Michael Walters <Michael1996Walters@gmail.com>
2023-07-05 10:47:49 +0100

    added tag model

--- server/models/Tag.js
+++ server/models/Tag.js
@@ -1,10 +1,11 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const tagSchema = new Schema(
-    {
-        name: {
-            type: String,
-        } 
+const tagSchema = new Schema({
+  name: {
+    type: String,
+  },
+});
 
-    }
-)
+const Tag = model("Tag", tagSchema);
+
+module.exports = Tag;

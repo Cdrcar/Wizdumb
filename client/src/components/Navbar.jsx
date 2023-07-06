f03ce429520102e4d7b439254e58db2c2b7fd059
Leesa A <leesaali7@gmail.com>
2023-07-06 10:40:57 +0100

    amended homepage styling and navbar, added animation

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React from "react";
+import { node } from "../assets";
 
 const Navbar = () => {
-    return (
-        <nav className=' className="sm:px-16 px-6 w-full flex items-center py-5 fixed top-0 z-20 bg-gradient-to-r from-emerald-500 to-lime-600"'>
-            <h1 className='font-bold'> WizDumb</h1>
-        </nav>
-    )
-  
-} ; 
+  return (
+    <nav className="bg-darkblue border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ">
+        <a href="/" className="flex items-center">
+          <img src={node} className="h-8 mr-3" alt="Wizdumb Logo" />
+          <span className="self-center text-2xl font-black whitespace-nowrap text-background">
+            Wiz
+            <span className="text-background">Dumb</span>
+          </span>
+        </a>
+        <div className="hidden w-full md:block md:w-auto">
+          <ul className="flex flex-col font-medium p-0 pr-4  mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
+            <li className="font-medium block py-2 pl-3 pr-4 text-background md:p-0 border-transparent hover:border-accent transition duration-250 border-b-2">
+              <a href="/">Courses</a>
+            </li>
+            <li className="font-medium block py-2 pl-3 pr-4 text-background md:p-0 border-transparent hover:border-accent transition duration-250 border-b-2">
+              <a href="/">Account</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,8 +3,16 @@ module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   mode: "jit",
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      background: "#fefefb",
+      lightblue: "#34b2d8",
+      darkblue: "#227498",
+      accent: "#ec354f",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};

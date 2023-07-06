3bd1cf9b31fe56c3310803f80bb75963da0783a9
[Iman Jama] <[Iman.jama891@gmail.com]>
2023-07-06 18:28:18 +0100

    changed errors with styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import courses from './constants/index.js';
 function App() {
   return (
     <BrowserRouter >
-      <div className='relative z-0 bg-sky-200 bg-cover bg-no-repeat bg-center'>
+      <div className='relative z-0 bg-cover bg-no-repeat bg-center'>
       <div>
         <Navbar />
         <Login />
@@ -16,7 +16,7 @@ function App() {
       </div>
       <div>
         
-        <h3 id="courses" className="flex flex-wrap mx-6 text-emerald-800 font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">Courses</h3>
+        <h3 id="courses" className=" font-black flex flex-wrap mx-6 text-cyan-700 md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">Courses</h3>
         <div className='grid grid-cols-3 gap-0'>
           {courses.map((course) => (
             <Course

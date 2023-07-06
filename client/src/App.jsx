64c9d8fafd9b8120e5b636db72b8ea57624a1817
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-07-06 21:25:35 +0100

    sign up, logo partially finished, logo made into element

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,47 +1,49 @@
 
 import './index.css';
 import { BrowserRouter } from "react-router-dom";
-import {AccountSettings, Course, DiscussionPage, Homepage, Navbar, LoggedinHomepage, Login, Modules, SearchPage, Footer} from './components'
+import { AccountSettings, Course, DiscussionPage, Homepage, Navbar, LoggedinHomepage, Login, Modules, SearchPage, Footer } from './components'
+import Signup from './components/Signup';
 import courses from './constants/index.js';
 
 function App() {
   return (
     <BrowserRouter >
       <div className='relative z-0 bg-cover bg-no-repeat bg-center'>
-      <div>
-        <Navbar />
-        <Login />
-        <Homepage />
+        <div>
+          <Navbar />
+          <Login />
+          <Signup />
+          <Homepage />
 
-      </div>
-      <div>
-        
-        <h3 id="courses" className=" font-black flex flex-wrap mx-6 text-cyan-700 md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">Courses</h3>
-        <div className='grid grid-cols-3 gap-0'>
-          {courses.map((course) => (
-            <Course
-              key={course.name}
-              name={course.name}
-              description={course.description}
-              icon={course.icon}
-              modules={course.modules}
-            />
-          ))}
+        </div>
+        <div>
+
+          <h3 id="courses" className=" font-black flex flex-wrap mx-6 text-cyan-700 md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">Courses</h3>
+          <div className='grid grid-cols-3 gap-0'>
+            {courses.map((course) => (
+              <Course
+                key={course.name}
+                name={course.name}
+                description={course.description}
+                icon={course.icon}
+                modules={course.modules}
+              />
+            ))}
           </div>
-      <LoggedinHomepage />
-      
-      <SearchPage />
-      <Modules />
-      <DiscussionPage />
-      <AccountSettings />
-      </div>
-       <div>
-       <Footer className='bg-lime-500'/>
-      </div>
+          <LoggedinHomepage />
+
+          <SearchPage />
+          <Modules />
+          <DiscussionPage />
+          <AccountSettings />
+        </div>
+        <div>
+          <Footer className='bg-lime-500' />
+        </div>
 
       </div>
-    
-      
+
+
     </BrowserRouter>
   );
 }
--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,26 +1,14 @@
 import React from "react";
 import { node } from "../assets";
 import brainLogo from "../assets/brainLogo.json";
-
 import Lottie from "lottie-react";
+import Logo from './Logo';
 
 const Homepage = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
-      <div className="grid grid-cols-3 absolute inset-0 top-[80px]  max-w-7xl mx-auto sm:px-16 px-6 flex flex-row items-start gap-5 justify-end">
-        <div className="mt-[-50px] inset-0">
-          <Lottie animationData={brainLogo} />
-        </div>
-        <div>
-          <h1 className="font-black lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2 text-sky-500 text-right mt-10">
-            Wiz<span className="text-cyan-700">Dumb</span>
-            <p className="font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[30px] ml-2 mt-5 mb-2 text-cyan-700 text-left">
-              Unlock Your Potential
-              <br className="sm:block" />
-              with Our Courses
-            </p>
-          </h1>
-        </div>
+      <div className="absolute inset-0 top-[80px]  max-w-7xl mx-auto sm:px-16 px-6 flex flex-row items-start gap-5 justify-end">
+        <Logo />
         <div className="pt-20">
           <ul className="text-center flex flex-col">
             <li

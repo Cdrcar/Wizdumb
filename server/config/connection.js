dc7785cbfc9171ab4360595ca5d479a530bcef9e
Michael Walters <106863927+MichaelW1996@users.noreply.github.com>
2023-07-11 18:33:39 +0100

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,27 +1,21 @@
-// require('dotenv').config();
-// const mongoose = require("mongoose");
+require('dotenv').config();
+const mongoose = require("mongoose");
 
-// mongoose
-//   .connect(process.env.MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then((db) => {
-//     const { host, name } = db.connections[0];
-//     console.log(`Successfully connected to the database at host ${host}`);
-//     console.log(`Database name: ${name}`);
-//   })
-//   .catch((error) => {
-//     console.error("Error connecting to the database", error);
-//     if (!process.env.MONGODB_URI) {
-//       console.error("process.env.MONGODB_URI is undefined. Please ensure your .env file is set up correctly and is not being ignored by git (should be in .gitignore file).");
-//     }
-//   });
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((db) => {
+    const { host, name } = db.connections[0];
+    console.log(`Successfully connected to the database at host ${host}`);
+    console.log(`Database name: ${name}`);
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
+    if (!process.env.MONGODB_URI) {
+      console.error("process.env.MONGODB_URI is undefined. Please ensure your .env file is set up correctly and is not being ignored by git (should be in .gitignore file).");
+  }
+ });
 
-// module.exports = mongoose.connection; // Export the Mongoose connection object
-
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/wizdumb');
-
-module.exports = mongoose.connection;
+module.exports = mongoose.connection; // Export the Mongoose connection object

94464540ddaf8f14b6a6abc180081fab9fd8ee5f
Faith Meades <faithmeades@faiths-air.home>
2023-07-12 13:51:00 +0100

    added search bar

--- client/src/components/LoggedinHomepage.jsx
+++ client/src/components/LoggedinHomepage.jsx
@@ -6,6 +6,8 @@ const LoggedinHomepage = () => {
   const [inProgressCount, setInProgressCount] = useState(0);
   const [courses, setCourses] = useState([]); // Placeholder for course data
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [currentSearch, setCurrentSearch] = useState([]);
 
   const handleCompletedIncrement = () => {
     setCompletedCount((prevCount) => prevCount + 1);
@@ -25,6 +27,22 @@ const LoggedinHomepage = () => {
     setSelectedCategory(category);
   };
 
+  const handleSearch = (e) => {
+    const searchQuery = e.target.value.toLowerCase();
+
+    e.preventDefault();
+    if (searchQuery === "") {
+      setCurrentSearch([]);
+      return false;
+    }
+    setCurrentSearch(
+      courses
+        .filter((course) => course.title.toLowerCase().includes(searchQuery))
+        .slice(0, 8)
+        .map((course) => course.title)
+    );
+  };
+
   const renderPlaceholderCards = () => {
     return (
       <div className="flex flex-col justify-end ml-5">
@@ -34,8 +52,6 @@ const LoggedinHomepage = () => {
       </div>
     );
   };
-
-  
 
   return (
     <div className="grid grid-cols-2 mt-10 ml-5 mr-5 center text-xl">
@@ -51,8 +67,6 @@ const LoggedinHomepage = () => {
             <div>Location</div>
           </div>
         </div>
-
-          
         <div>
           <div className="flex justify-center mx-auto space-x-4 mt-20 mb-20">
             <div
@@ -69,6 +83,31 @@ const LoggedinHomepage = () => {
               <span className="inline-block w-10 h-4 rounded-full bg-red-500"></span>
               <span className="ml-2">In Progress ({inProgressCount})</span>
             </div>
+          </div>
+
+          <div className="flex justify-center items-center mb-20">
+            <form className="w-[500px] relative">
+              <div className="relative">
+                <div className="bg-gradient-to-r from-gray-400 via-gray-600 to-blue-800 p-1 rounded-full border-xl">
+                  <input
+                    type="search"
+                    placeholder="Search Here"
+                    className="w-full rounded-full p-4 bg-slate-200 text-black"
+                    onChange={handleSearch}
+                  />
+                  <button className="absolute right-1 top-1/2 -translate-y-1/2 p-4 bg-cyan-300 bg-opacity-25 rounded-full mx-2">
+                    Search
+                  </button>
+                </div>
+              </div>
+              {currentSearch.length > 0 && (
+                <div className="absolute top-20 p-4 bg-blue-100 text-black font-bold w-full rounded-xl left-1/2 -translate-x-1/2 flex flex-col gap-2 hover:pointer-cursor">
+                  {currentSearch.map((search) => (
+                    <span key={search}>{search}</span>
+                  ))}
+                </div>
+              )}
+            </form>
           </div>
         </div>
       </div>

0b6ffa81fb6b98e981e5dfbcbddac3b6a61e40c5
Leesa A <leesaali7@gmail.com>
2023-07-13 02:31:28 +0100

    added login/logout functionality

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./store";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-   document.getElementById('root')
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    ,
+  </Provider>,
+  document.getElementById("root")
 );
-
-
---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authReducer = createSlice({
+  name: "auth",
+  initialState: {
+    isLoggedIn: false,
+    user: { token: "", username: "", userId: "" },
+  },
+  reducers: {
+    login(state, action) {
+      return {
+        ...state,
+        isLoggedIn: true,
+        user: {
+          token: action.payload.token,
+          username: action.payload.username,
+          userId: action.payload.userId,
+        },
+      };
+    },
+
+    logout(state) {
+      return {
+        ...state,
+        isLoggedIn: false,
+        user: { token: "", username: "", userId: "" },
+      };
+    },
+  },
+});
+
+export const { login, logout } = authReducer.actions;
+
+export default authReducer.reducer;

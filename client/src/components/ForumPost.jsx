1f4067503e00a6a0af5f28d185ffa5e791200bbe
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-07-16 18:19:44 +0100

    update branch

--- client/src/components/ForumPost.jsx
+++ client/src/components/ForumPost.jsx
@@ -14,9 +14,15 @@ const ForumPost = () => {
     const { loading, error, data } = useQuery(QUERY_SINGLE_COMMENT, {
         variables: { getCommentId: currentId }
     });
-    // console.log('Data:', data)
-    const comments = data?.getComment || [];
-    console.log('Comments:', comments);
+    
+    let comments = { comment: "No Post", title: "No Title", commentn: [], like: [], user: { username: "No User"}, _id: "No Id"};
+    let comment2 = [];
+    if (!loading) {
+        comments = data?.getComment || [];
+        comment2 = comments.commentn;
+      }
+      console.log(comment2)
+      console.log(comments)
 
     // const likePost = (e) => {
 
@@ -24,7 +30,7 @@ const ForumPost = () => {
 
 
     return (
-        <div className='bg-slate-100'>
+        <div className='bg-slate-100 pb-10'>
             <h1 className='text-6xl font-bold text-center text-cyan-800 pt-24 pb-16'>Forum</h1>
             <div className='w-full md:flex'>
                 <div className='grid justify-items-center mb-5 md:mb-0 md:order-last md:justify-self-end md:mr-5'>
@@ -40,20 +46,17 @@ const ForumPost = () => {
                 </div>
                 <div className='w-full'>
                     <div className='grid mx-5 border-2 border-slate-500 bg-slate-300 rounded-lg'>
-                        <h3 className='mt-3 mx-3 font-bold'>Post Title</h3>
-                        <p className='px-2 pb-3 font-medium'>
-                            JavaScript is a lightweight, cross-platform, interpreted scripting language. It is well-known for the development of web pages, many non-browser environments also use it. JavaScript can be used for Client-side developments as well as Server-side developments.
-                            In this article, we need to create a Random String Generator using Javascript that will generate a random set of strings & get displayed when the user clicks a button. Additionally, we can also take input for the length of the string from the user. On the client-side, it will render those strings once generated using the Document Object Model (DOM) concept.
-                            Approach: For generating Random Strings in Javascript, we can use in-built methods that will actually generate the strings and can manipulate the Document Object Model (DOM) accordingly. We will use the Math library to access the random() function that will help to generate the random index and multiple with the length of the string ie., it will append the character from the string or character set as it is passed.                         </p>
+                        <h3 className='mt-3 mx-3 font-bold'>{comments.title}</h3>
+                        <p className='px-2 pb-3 font-medium'>{comments.comment}</p>
                         <div className='grid flex-none inline pr-1 sm:pr-2 items-center justify-self-end'>
                             <div className='flex flex-wrap ml-3 mb-1'>
                                 <div className='flex flex-wrap items-center mr-3'>
                                     <BiLike className='fill-green-600' />
-                                    <p className='text-green-600'></p>
+                                    <p className='text-green-600'>{comments.commentn.length}</p>
                                 </div>
                                 <div className='flex flex-wrap items-center '>
                                     <BiComment className='fill-yellow-500' />
-                                    <p className='text-yellow-500'>5</p>
+                                    <p className='text-yellow-500'>{comments.like.length}</p>
                                 </div>
                             </div>
                         </div>
@@ -61,12 +64,13 @@ const ForumPost = () => {
                 </div>
             </div>
             <div className='mb-5 mx-5'>
-                <ForumComment />
-                <ForumComment />
-                <ForumComment />
-                <ForumComment />
-                <ForumComment />
-                <ForumComment />
+            {comment2.map((reply) => (
+            <ForumComment
+              title={reply.title}
+              like={reply.reply}
+              key={reply.id}
+            />
+          ))}
             </div>
         </div>
     )

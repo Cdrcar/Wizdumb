0447fa410968e02d055df51d49bebd34f9998ec4
Carmela <carmela881@outlook.com>
2023-07-16 21:56:52 +0100

    updated style footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,28 +1,41 @@
-import { FaGithub } from 'react-icons/fa';
+import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 const Footer = () => {
+  const year = new Date().getFullYear();
+  return (
+    <footer className="text-gray-900 mb-0 bg-white bg-opacity-90 border-t-0 mt-10 left-0 w-full z-0">
+      <div className="py-10">
+        <div className="container mx-auto flex flex-col md:flex-row justify-between items-start">
+          <div className="flex flex-col items-start">
+            <h2 className="text-base font-bold mb-4 text-sky-500">Wiz<span className="text-rose-600">Dumb</span></h2>
+            <p className="text-xs">A learning platform for everyone.</p>
+          </div>
 
-
-  const year = new Date().getFullYear(); 
-    return (
-         <div className='contact bg-gradient-to-r from-blue-100 via-blue-300 to-blue-500 z-0 '>
-                 
-                <a href="https://github.com/Cdrcar/Wizdumb">
-                    <button
-                        type="button"
-                        className="  inline-block rounded-full px-4 py-1.5 text-l text-white bg-zinc-800 hover:bg-zinc-400">
-                        <FaGithub />
-                    </button>
-                </a>
-                
-            <p className='hover:text-sm'><a href="#">Back to top</a></p>
-            <p className="text-black font-bold">
-               &copy; WizDumb 👨‍💻 {year}
-            </p> 
-
+          <div className="flex items-center mt-6 md:mt-0">
+            <a
+              href="https://github.com/Cdrcar/Wizdumb"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-900 text-sm hover:text-blue-300 ml-6"
+            >
+              <FaGithub className="inline-block mr-1 text-lg" />
+              GitHub
+            </a>
+          </div>
         </div>
-  
-           
-)};
-       
-   
+      </div>
+
+      <div className="container mx-auto border-t border-gray-800 py-4">
+        <div className="flex flex-wrap justify-center text-sm">
+          <Link to="/" className="text-gray-900 mx-4 my-1 hover:text-blue-300">Homepage</Link>
+        </div>
+        <p className="text-xs text-center mt-4">
+          &copy; {year} WizDumb. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
 export default Footer;
